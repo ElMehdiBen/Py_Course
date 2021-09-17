f9e16026fd2f9a72fef279f5135d3cc224bb8a38
El Mehdi Benyoussef <elmehdi.benyoussef@publicisgroupe.com>
2021-09-17 16:05:06 +0100

    updating cassandra file

--- Cassandra.py
+++ Cassandra.py
@@ -1,9 +1,20 @@
-from cassandra.cluster import Cluster
+# ETL using Pandas and Cassandra
 
-if __name__ == "__main__":
-    cluster = Cluster(['209.188.7.148'],port=9042)
-    session = cluster.connect('data', wait_for_all_pools=True)
-    session.execute('USE data')
-    rows = session.execute('SELECT * FROM users')
-    for row in rows:
-        print(row.age,row.name,row.username)
+from cassandra.cluster import Cluster
+import pandas as pd
+
+# Extract Data
+
+cluster = Cluster(['209.188.7.148'],port=9042)
+session = cluster.connect('mehdi', wait_for_all_pools=True)
+session.execute('USE mehdi')
+rows = session.execute('SELECT * FROM data')
+############################################
+# Transform Data
+data = pd.DataFrame(rows)
+
+data
+
+############################################
+# Load Data
+# Write data to Cassandra - Creating new table
