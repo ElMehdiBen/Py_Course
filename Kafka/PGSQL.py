99d852879fb40ec684dd020bf8fa2ce416f38f2d
El Mehdi Benyoussef <elmehdibenyoussef@gmail.com>
2021-10-28 13:06:37 +0100

    Create PGSQL.py

---
+++ Kafka/PGSQL.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+
+connection = psycopg2.connect(user="postgres",
+                              password="123456789",
+                              host="209.188.7.148",
+                              port="5432",
+                              database="data")
+cursor = connection.cursor()
+cursor.execute("select * from public.customers")
+
+# fetchone is used to get line by line from PGSQL
+row = cur.fetchone()
+
+# fetchall is used to get all the lines at once
+DF = pd.DataFrame(cursor.fetchall())
