6881659639ebe92d61e5926a4b19db1a9955502d
El Mehdi Benyoussef <elmehdibenyoussef@gmail.com>
2021-10-28 10:10:08 +0100

    Update Prod.py

--- Kafka/Prod.py
+++ Kafka/Prod.py
@@ -1,11 +1,14 @@
 from time import sleep
 from json import dumps
+from random import choice
 from kafka import KafkaProducer
 
 producer = KafkaProducer(bootstrap_servers = ['209.188.7.148:9092'],
                          value_serializer = lambda v: dumps(v).encode('utf-8'))
 
+receivers = ["Olivier", "Charlene", "Laurent", "Noura"]
+
 for e in range(1000):
-    data = {'number' : e}
-    producer.send('numtest', value = data)
-    sleep(5)
+    data = {'sender': "Mehdi", 'receiver': choice(receivers), 'number' : e}
+    producer.send('messages', value = data)
+    sleep(1)
