7f712dba7605b5485692fae95bf8451ce25f1d97
El Mehdi Benyoussef <elmehdibenyoussef@gmail.com>
2021-10-28 10:29:24 +0100

    Update Prod.py

--- Kafka/Prod.py
+++ Kafka/Prod.py
@@ -6,9 +6,10 @@ from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers = ['209.188.7.148:9092'],
                          value_serializer = lambda v: dumps(v).encode('utf-8'))
 
-receivers = ["Olivier", "Charlene", "Laurent", "Noura"]
+receivers = ["Olivier", "Charlene", "Laurent", "Noura", "Malick", "Anne-Cha", "Pascal", "Zeyno", "Ludwig", "Tristan", "Camille"]
 
 for e in range(1000):
     data = {'sender': "Mehdi", 'receiver': choice(receivers), 'number' : e}
+    print('{}'.format(data))
     producer.send('messages', value = data)
-    sleep(1)
+    sleep(3)
